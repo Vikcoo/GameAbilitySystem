809b48fa1cbf327e22edcefb22e4a2f1e175dbc8
Vikcoo <1363370470@qq.com>
2024-04-07 19:15:04 +0800

    playerController end

--- Source/GameAbilitySystem/Private/Player/AuraPlayerController.cpp
+++ Source/GameAbilitySystem/Private/Player/AuraPlayerController.cpp
@@ -1,10 +1,6 @@
 // Copyright yu kai
-
-
+#include "EnhancedInputComponent.h"
 #include "Player/AuraPlayerController.h"
-#include "Player/AuraPlayerController.h"
-
-#include "K2Node_GetSubsystem.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -30,4 +26,30 @@ void AAuraPlayerController::BeginPlay()
 
 
 	
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	//将moveAction 和 move方法绑定  调用方法时候会自动将我们在action中相同类型的数值  通过FInputActionValue参数传入。
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this, &AAuraPlayerController::Move);
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	//获取方向  ？？只能说很不直观
+	const FVector2d InputAxisVector =  InputActionValue.Get<FVector2d>();
+	const FRotator Rotation = GetControlRotation();
+	const FRotator YawRotation = FRotator(0, Rotation.Yaw, 0);
+	FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	//将方向 和 倍率 输入    倍率可以控制正负
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		ControlledPawn->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
 }
--- Source/GameAbilitySystem/Public/Player/AuraPlayerController.h
+++ Source/GameAbilitySystem/Public/Player/AuraPlayerController.h
@@ -18,7 +18,11 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 private:
 	UPROPERTY(EditAnywhere,Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+	UPROPERTY(EditAnywhere,Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	void Move(const FInputActionValue& InputActionValue);
 };
