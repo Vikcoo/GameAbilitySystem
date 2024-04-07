7b7cf8908ec132af3b2d7a41d71381b5ed3e0865
Vikcoo <1363370470@qq.com>
2024-04-07 17:08:31 +0800

    Enhanced Input IA_Move,IMC and new controller Class

---
+++ Source/GameAbilitySystem/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,5 @@
+// Copyright yu kai
+
+
+#include "Player/AuraPlayerController.h"
+
---
+++ Source/GameAbilitySystem/Public/Player/AuraPlayerController.h
@@ -0,0 +1,17 @@
+// Copyright yu kai
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "AuraPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEABILITYSYSTEM_API AAuraPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
