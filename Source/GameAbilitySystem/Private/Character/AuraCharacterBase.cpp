0706bba39652a08f4682ebdd5b85085809580018
Vikcoo <1363370470@qq.com>
2024-04-07 16:23:24 +0800

    完善主角和长毛哥布林蓝图，设置mesh，武器Socket

--- Source/GameAbilitySystem/Private/Character/AuraCharacterBase.cpp
+++ Source/GameAbilitySystem/Private/Character/AuraCharacterBase.cpp
@@ -6,13 +6,16 @@
 AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
-
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
 }
 
 void AAuraCharacterBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 }
 
 
