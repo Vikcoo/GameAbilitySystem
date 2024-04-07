99fa1d305bd0db2d3aba316be708d0178a2b1e06
Vikcoo <1363370470@qq.com>
2024-04-07 18:40:25 +0800

    PlayerController

--- Source/GameAbilitySystem/Public/Player/AuraPlayerController.h
+++ Source/GameAbilitySystem/Public/Player/AuraPlayerController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputSubsystems.h"
 #include "AuraPlayerController.generated.h"
 
 /**
@@ -13,5 +14,11 @@ UCLASS()
 class GAMEABILITYSYSTEM_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere,Category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
 };
