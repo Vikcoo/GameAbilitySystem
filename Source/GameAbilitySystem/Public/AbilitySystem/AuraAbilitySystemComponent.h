d3b6343dbc2c656a551086b67e47fa5e7f337fe6
Vikcoo <1363370470@qq.com>
2024-04-08 10:40:28 +0800

    import ASC and AS ,create class

---
+++ Source/GameAbilitySystem/Public/AbilitySystem/AuraAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Copyright yu kai
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AuraAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEABILITYSYSTEM_API UAuraAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/GameAbilitySystem/Public/AbilitySystem/AuraAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright yu kai
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AuraAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEABILITYSYSTEM_API UAuraAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
