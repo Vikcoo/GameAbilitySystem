b44e28c5478f6851c3c639ff9dce43f4a652ed6c
Vikcoo <1363370470@qq.com>
2024-04-09 17:02:15 +0800

    Periodic Gameplay Effects

--- Source/GameAbilitySystem/Public/Actor/AuraEffectActor.h
+++ Source/GameAbilitySystem/Public/Actor/AuraEffectActor.h
@@ -23,4 +23,7 @@ protected:
 	
 	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effects")
 	TSubclassOf<UGameplayEffect> InstantGamePlayEffectClass;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effects")
+	TSubclassOf<UGameplayEffect> DurationGamePlayEffectClass;
 };
